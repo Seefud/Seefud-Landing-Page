decea9954b828ba9db1295b37470a995730006bf
RafliOrtegaJaya <m299b4ky3606@bangkit.academy>
2024-12-02 17:47:39 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function toggleNavbar() {
+    const navbar = document.getElementById('navbar');
+    const closeIcon = document.getElementById('closeIcon');
+    navbar.classList.toggle('active');
+    closeIcon.classList.toggle('active');
+  }
